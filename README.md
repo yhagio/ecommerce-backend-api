e1cd170df4dcfdd7ef5bf7869cad606f74326c67
Yuichi Hagio <yhagio87@gmail.com>
2016-10-12 23:07:34 -0400

    Adding routes

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ npm run start
 psql -l
 psql -d postpg -c "SELECT * FROM todos"
 ```
+
+| APIs | REST | 
+| --- |---| 
+| /api/products | GET | 
+| /api/products/:id | GET | 
+| /api/admin/products/ | POST | 
+| /api/admin/products/:id/ | PUT / DELETE | 
+| /api/signup | POST | 
+| /api/login | POST | 
+| /api/logout | GET | 
+| /api/reset_password | POST | 
+| /api/account | GET / PUT / DELETE | 
+| /api/account/invoices | GET | 
+
--- index.js
+++ index.js
@@ -24,14 +24,72 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
-const Todo = sequelize.define('todos', {
+const Product = sequelize.define('products', {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  text: {
+  name: {
     type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.FLOAT
+  },
+  description: {
+    type: Sequelize.STRING
+  }
+});
+
+const User = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  first_name: {
+    type: Sequelize.STRING
+  },
+  last_name: {
+    type: Sequelize.STRING
+  },
+  address: {
+    type: Sequelize.STRING
+  },
+  photo_url: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  }
+});
+
+const Review = sequelize.define('reviews', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  product_id: {
+    type: Sequelize.STRING
+  },
+  user_id: {
+    type: Sequelize.STRING
+  },
+  body: {
+    type: Sequelize.STRING
+  },
+  rating: {
+    type: Sequelize.INTEGER
   }
 });
 
@@ -56,10 +114,26 @@ app.use(helmet());
 app.use(cors());
 
 // Routes
-app.route('/api/todos')
+app.route('/api/products')
   .get((req, res) => {
-    Todo.findAll().then((todos) => {
-      res.json(todos);
+    Product.findAll().then((products) => {
+      res.json(products);
+    });
+
+  });
+
+app.route('/api/admin/products')
+  .post((req, res) => {
+    const productObject = {
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description
+    };
+
+    Product.create(productObject).then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      console.log('Error on post: ', err);
     });
   });
 
