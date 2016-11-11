bdfde1b5daeba4f7a0a0347e6c772163643da685
Yuichi Hagio <yhagio87@gmail.com>
2016-11-11 00:14:28 -0500

    Update user, new user, create receipt

--- server/api/payment/controller.js
+++ server/api/payment/controller.js
@@ -1,6 +1,7 @@
 const Model = require('../../../models');
 const User = Model.User;
 const Receipt = Model.Receipt;
+const CartItem = Model.CartItem;
 
 const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
@@ -23,46 +24,7 @@ exports.payTotal = (req, res) => {
 
   // WHEN USER EXISTS ALREADY
   if (req.user.stripe_customer_id) {
-    console.log('++++++++++ WELCOME BACK\n\n\n');
     // 1. Payment
-    // const receipts = [];
-
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    // function chargeItem(item) {
-    //   console.log("ITEM=========>\n");
-    //   // return new Promise(
-    //   return stripe.charges.create({
-    //             amount: item.product_price * 100, // Amount in cents
-    //             currency: 'cad',
-    //             customer: req.user.stripe_customer_id,
-    //             description: item.product_name,
-    //           })
-    //           .then((charge) => {
-    //             console.log('CHARGED:::\n');
-    //             // TODO: save each charge to Purchase Hisotry
-    //             Receipt.create({
-    //               user_id: req.user.id,
-    //               product_id: item.product_id,
-    //               stripe_charge_id: charge.id,
-    //               stripe_customer_id: req.user.stripe_customer_id,
-    //               price: item.product_price,
-    //               description: item.product_name,
-    //             })
-    //             .then((receipt) => {
-    //               console.log('RECEIPT \n');
-    //               // receipts.push(receipt);
-    //             }).catch((err2) => {
-    //               console.log('ERR2 \n', err2);
-    //               return res.status(400).send('Payment failed.');
-    //             });
-    //           })
-    //           .catch((err3) => {
-    //             console.log('Charge failed \n', err3);
-    //             return res.status(400).send('Payment failed.');
-    //           });
-    //     // );
-    // }
-
     const handleEachItem = (list) => {
       const promises = list.map((item) => {
         return stripe.charges.create({
@@ -72,8 +34,7 @@ exports.payTotal = (req, res) => {
           description: item.product_name,
         })
         .then((charge) => {
-          console.log('CHARGED:::\n');
-          // TODO: save each charge to Purchase Hisotry
+          // 2. Save each charge to Purchase Hisotry
           return Receipt.create({
             user_id: req.user.id,
             product_id: item.product_id,
@@ -81,74 +42,93 @@ exports.payTotal = (req, res) => {
             stripe_customer_id: req.user.stripe_customer_id,
             price: item.product_price,
             description: item.product_name,
-          })
-          // .then((receipt) => {
-          //   // console.log('CHARGE \n', charge);
-          //   // console.log('RECEIPT \n', receipt);
-          //   // receipts.push(receipt);
-          //   return receipt;
-          // }).catch((err2) => {
-          //   console.log('ERR2 \n', err2);
-          //   return res.status(400).send('Payment failed.');
-          // });
-        })
-        // .catch((err3) => {
-        //   console.log('Charge failed \n', err3);
-        //   return res.status(400).send('Payment failed.');
-        // });
+          });
+        });
       });
       return Promise.all(promises);
     };
 
-    // Promise.all(purchaseList.forEach(chargeItem))
+    // Handle charge + create receipt for each cart item
     handleEachItem(purchaseList)
     .then((result) => {
-      console.log('RESULT:\n', result);
-      // console.log('Items \n', items);
-      // console.log('Charges \n', charges);
-      return res.status(200).send(result);
-    }).catch((error) => {
-      console.log('ERROR PROMISE \n', error);
-    });
-
-    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    /*
-    purchaseList.forEach((item) => {
-      stripe.charges.create({
-        amount: item.product_price * 100, // Amount in cents
-        currency: 'cad',
-        customer: req.user.stripe_customer_id,
-        description: item.product_name,
-      }).then((charge) => {
-        // TODO: save each charge to Purchase Hisotry
-        Receipt.create({
+      // 3. Empty the cart after successfully paid
+      return CartItem.destroy({
+        where: {
           user_id: req.user.id,
-          product_id: item.product_id,
-          stripe_charge_id: charge.id,
-          stripe_customer_id: req.user.stripe_customer_id,
-          price: item.product_price,
-          description: item.product_name,
-        }).then((receipt) => {
-          receipts.push(receipt);
-        }).catch((err2) => {
-          console.log('ERR2 \n', err2);
-          return res.status(400).send('Payment failed.');
-        });
-
-      }).catch((err3) => {
-        console.log('Charge failed \n', err3);
-        return res.status(400).send('Payment failed.');
+        },
       });
-    });
-
-    console.log('SAVED receipts \n', receipts);
-    return res.status(200).send(receipts);
-    */
-    // 2. Create Receipt for each product
+    })
+    .then(data => res.sendStatus(200))
+    .catch(error => res.status(400).send(error));
 
   // WHEN NEW CUSTOMER PAYS
   } else {
-    console.log('++++++++++ NEW USER\n\n\n');
+    // 1. New user setup
+    let customerId;
+
+    const handleNewCustomer = () => {
+      return stripe.customers.create({
+        source: token,
+        description: 'Lovely ECommerce Customer',
+        email: req.user.email,
+      }).then((customer) => {
+        req.user.stripe_customer_id = customer.id;
+        customerId = customer.id;
+        return User.update(
+          {
+            stripe_customer_id: customer.id
+          }, {
+            where: { id: req.user.id }
+          });
+      });
+    };
+
+    // 2. handle paymeny and purchase history
+    const handlePurchaseAndReceipt = (list) => {
+      const promises = list.map((item) => {
+        return stripe.charges.create({
+          amount: item.product_price * 100, // Amount in cents
+          currency: 'cad',
+          customer: req.user.stripe_customer_id,
+          description: item.product_name,
+        })
+        .then((charge) => {
+          // 2. Save each charge to Purchase Hisotry
+          return Receipt.create({
+            user_id: req.user.id,
+            product_id: item.product_id,
+            stripe_charge_id: charge.id,
+            stripe_customer_id: req.user.stripe_customer_id || customerId,
+            price: item.product_price,
+            description: item.product_name,
+          });
+        });
+      });
+      return Promise.all(promises);
+    };
+
+    handleNewCustomer()
+    .then((user) => {
+    // Handle charge + create receipt for each cart item
+      return handlePurchaseAndReceipt(purchaseList)
+      .then((result) => {
+        // 3. Empty the cart after successfully paid
+        return CartItem.destroy({
+          where: {
+            user_id: req.user.id,
+          },
+        });
+      })
+      .then(data => res.sendStatus(200))
+      .catch(error => res.status(400).send(error));
+    })
+    .catch(err => res.status(400).send('Payment failed.'));
+
+
+
+    /*
+
+
     // 1. Create a customer
     stripe.customers.create({
       source: token,
@@ -229,5 +209,7 @@ exports.payTotal = (req, res) => {
       console.log('>>>>>> ERROR STRIPE >>>>>>: \n', err);
       return res.status(400).send('Payment failed.');
     });
+
+    */
   }
 };
