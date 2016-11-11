18f133a7f6787329b0b9c0e9947734dc9afcc302
Yuichi Hagio <yhagio87@gmail.com>
2016-11-11 00:17:26 -0500

    Removed some comments

--- server/api/payment/controller.js
+++ server/api/payment/controller.js
@@ -15,14 +15,14 @@ exports.payTotal = (req, res) => {
     return res.status(403).send({ error: 'Not authorized.' });
   }
 
-  // console.log('>>> BODY >>> \n', req.body);
-
   const token = req.body.token;
   const purchaseList = req.body.purchaseList;
 
   // CHECK IF THE USER PAID BEFORE
 
-  // WHEN USER EXISTS ALREADY
+  /*****************************
+   *  WHEN EXISTING CUSTOMER (PURCHASED BEFORE) PAYS
+   *****************************/
   if (req.user.stripe_customer_id) {
     // 1. Payment
     const handleEachItem = (list) => {
@@ -61,7 +61,9 @@ exports.payTotal = (req, res) => {
     .then(data => res.sendStatus(200))
     .catch(error => res.status(400).send(error));
 
-  // WHEN NEW CUSTOMER PAYS
+  /*****************************
+   *  WHEN NEW CUSTOMER PAYS
+   *****************************/
   } else {
     // 1. New user setup
     let customerId;
@@ -123,93 +125,5 @@ exports.payTotal = (req, res) => {
       .catch(error => res.status(400).send(error));
     })
     .catch(err => res.status(400).send('Payment failed.'));
-
-
-
-    /*
-
-
-    // 1. Create a customer
-    stripe.customers.create({
-      source: token,
-      description: 'Lovely ECommerce Customer',
-      email: req.user.email,
-    }).then((customer) => {
-      console.log('>>>>> CUSTOMER >>>>\n', customer);
-
-      // TODO: update User to add stripe_customer_id
-      if (!req.user.stripe_customer_id) {
-        User.update(
-          {
-            stripe_customer_id: customer.id
-          }, {
-            where: { id: req.user.id }
-          })
-          .then((user) => {
-            console.log('Updated user', user);
-            req.user = user;
-          })
-          .catch((err1) => {
-            return res.status(400).send('Failed to update User.');
-          });
-      }
-
-      // 2. Charge each product purchase
-      // and save Receipt & Purchased
-      const receipts = [];
-
-      purchaseList.forEach((item) => {
-        stripe.charges.create({
-          amount: item.product_price, // Amount in cents
-          currency: 'cad',
-          customer: customer.id,
-          description: item.product_name,
-        }).then((charge) => {
-          // 3.
-          // TODO: save each charge to Purchase Hisotry
-          Receipt.create({
-            user_id: req.user.id,
-            product_id: item.product_id,
-            stripe_charge_id: charge.id,
-            stripe_customer_id: req.user.stripe_customer_id,
-            price: item.product_price,
-            description: item.product_name,
-          }).then((receipt) => {
-            receipts.push(receipt);
-          }).catch((err2) => {
-            console.log('ERR2 \n', err2);
-            return res.status(400).send('Payment failed.');
-          });
-
-        }).catch((err3) => {
-          console.log('Charge failed \n', err3);
-          return res.status(400).send('Payment failed.');
-        });
-      });
-
-      return receipts;
-    }).then((charges) => {
-      console.log('>>>>>> CHARGE >>>>>>: \n', charges);
-
-      // 4. Return all receipts for the customer
-      // TODO
-
-      // REMOVED later:
-      // Just to check: Retrieve invoices
-      // stripe.invoices.list({ customer: customerId })
-      //   .then((invoices) => {
-      //     console.log('>>>>> Invoices >>>> \n', invoices);
-      //   })
-      //   .catch((invoiceErr) => {
-      //     console.log('>>>>> Invoice Err >>> \n', invoiceErr);
-      //   });
-
-      return res.status(200).send('Paid successfully.');
-    }).catch((err) => {
-      console.log('>>>>>> ERROR STRIPE >>>>>>: \n', err);
-      return res.status(400).send('Payment failed.');
-    });
-
-    */
   }
 };
